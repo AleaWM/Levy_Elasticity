07aac0fadcfcd3d4662e077289c1aca321d8949d
Michael <mvanhu2@uic.edu>
2024-11-07 21:50:30 -0600

    More graphs.

--- nta_agency_type_bar_charts.R
+++ nta_agency_type_bar_charts.R
@@ -1,16 +1,14 @@
 ## Bar Charts for NTA ##
 
 library(tidyverse)
-library(scales)
-library(RColorBrewer)
 
 ## All agencies
 
 df |>
   filter(year == 2022) |>
   mutate(type = ifelse(type == "Muni", "Municipality", type)) |>
-  ggplot(aes(x = type, fill = type)) +
-  geom_bar() +
+  ggplot(aes(x = type)) +
+  geom_bar(fill = "blue4") +
   geom_text(stat = 'count', aes(label = ..count.., y = ..count..), vjust = -0.5, color = "black") +
   labs(
     title = "Taxing Agency Count by Agency Type",
@@ -19,7 +17,6 @@ df |>
     y = "N"
   ) +
   theme_classic() +
-  scale_fill_manual(values = brewer.pal(n = 6, "Blues")[3:6]) +
   theme(legend.position = "none",
         axis.title.y = element_text(angle = 0))
 
@@ -30,7 +27,7 @@ df |>
   filter(type == "School") |>
   filter(minor_type %in% c("ELEMENTARY", "SECONDARY")) |>
   ggplot(aes(x = minor_type, fill = minor_type)) +
-  geom_bar() +
+  geom_bar(fill = "blue4") +
   geom_text(stat = 'count', aes(label = ..count.., y = ..count..), vjust = -0.5, color = "black") +
   labs(
     title = "Schools: Primary and Secondary",
@@ -39,7 +36,7 @@ df |>
     y = "N"
   ) +
   theme_classic() +
-  scale_fill_manual(values = c(brewer.pal(n = 6, "Blues")[3], brewer.pal(n = 6, "Blues")[5])) +  # Specify colors individually
+  #scale_fill_manual(values = c(brewer.pal(n = 6, "Blues")[3], brewer.pal(n = 6, "Blues")[5])) +  # Specify colors individually
   theme(legend.position = "none",
         axis.title.y = element_text(angle = 0))
 
@@ -50,7 +47,7 @@ df |>
   filter(type == "Muni") |>
   mutate(home_rule = ifelse(home_rule_ind == 1, "Yes", "No")) |>  # Create a new column for home rule status
   ggplot(aes(x = home_rule, fill = home_rule)) +  # Use the new column for x-axis
-  geom_bar() +
+  geom_bar(fill = "blue4") +
   geom_text(stat = 'count', aes(label = ..count.., y = ..count..), vjust = -0.5, color = "black") +
   labs(
     title = "Municipalities by Home Rule Status",
@@ -59,6 +56,56 @@ df |>
     y = "N"
   ) +
   theme_classic() +
-  scale_fill_manual(values = brewer.pal(n = 5, "Blues")[3:4]) +  # Use 2 colors from the Blues palette
   theme(legend.position = "none",
-        axis.title.y = element_text(angle = 0)) 
+        axis.title.y = element_text(angle = 0))
+
+## Histograms for NTA?
+
+# All agencies
+
+df |>
+  filter(year %in% c("2012", "2022")) |>
+  ggplot(aes(x = (cty_total_eav/1e+06))) +
+  geom_histogram(fill = "blue4") +
+  facet_grid(cols = vars(year)) +
+  labs(
+    title = "Taxing Agency EAV",
+    subtitle = "2012 and 2022",
+    x = "$ (millions)",
+    y = "N"
+  ) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.y = element_text(angle = 0))
+
+df |>
+  filter(year %in% c("2012", "2022")) |>
+  ggplot(aes(x = (total_final_levy/1e+06))) +
+  geom_histogram(fill = "blue4") +
+  facet_grid(cols = vars(year)) +
+  labs(
+    title = "Taxing Agency EAV",
+    subtitle = "2012 and 2022",
+    x = "$ (millions)",
+    y = "N"
+  ) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.y = element_text(angle = 0))
+
+# Munis
+
+df |>
+  filter(year %in% c("2012", "2022")) |>
+  ggplot(aes(x = (cty_total_eav/1e+06))) +
+  geom_histogram(fill = "blue4") +
+  facet_grid(cols = vars(year), rows = vars(home_rule_ind)) +
+  labs(
+    title = "Taxing Agency EAV",
+    subtitle = "2012 and 2022",
+    x = "$ (millions)",
+    y = "N"
+  ) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.y = element_text(angle = 0))
